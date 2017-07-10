9750ef54bceceb16081c689b7af9ac4ad8cdc160
sidewinder2020 <sdwinder1@gmail.com>
2017-07-10 17:39:04 -0600

    Create offsets class with find last 4 digits method

--- lib/offsets.rb
+++ lib/offsets.rb
@@ -1,9 +1,27 @@
 require 'pry'
-
+require './lib/encryption'
+require './lib/key'
 
 class Offsets
-attr_accessor
+attr_accessor :date
 
   def initialize
-    @date = 
+    @date = '020315'
+    #DDMMYY
   end
+
+  # def date_squared
+  #   @date.to_i ** 2
+  # end
+
+#find the last 4 digits
+  def find_last_4_digits
+    date_squared = @date.to_i ** 2
+    date_array = date_squared.to_s.split(//)
+    date_array[-4..-1]
+  end
+
+end
+
+
+#1st digit is the a offset, etc.
--- test/offsets_test.rb
+++ test/offsets_test.rb
@@ -4,3 +4,13 @@ require 'minitest/pride'
 require './lib/offsets'
 
 class OffsetsTest < Minitest::Test
+
+  def setup
+    @off = Offsets.new
+  end
+
+  def test_it_returns_last_4_numbers
+    assert_equal ["9","2","2","5"], @off.find_last_4_digits
+  end
+
+end
