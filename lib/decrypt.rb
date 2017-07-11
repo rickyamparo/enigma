2e6176c78959a9b2a63a56cd5019fad552483207
sidewinder2020 <sdwinder1@gmail.com>
2017-07-11 12:00:38 -0600

    Create split and index value method

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,11 +1,26 @@
 require 'pry'
 require './lib/key'
 require './lib/offsets'
+require './lib/encrypt'
 
 class Decrypt
+attr_accessor :encrypt, :key, :offsets
 
   def initialize
-    @message = ",ash"
+    @encrypt = Encrypt.new
+    @output_message = ",ash"
+  end
+
+  def split_decryption_string
+    local_array = @output_message.split(//)
+  end
+
+  def assign_index_value
+    local_array = split_decryption_string
+    encrypt.create_array
+    local_array.map do |letter|
+      encrypt.map_values_array.index(letter)
+    end
   end
 
 end
--- test/decrypt_test.rb
+++ test/decrypt_test.rb
@@ -9,6 +9,12 @@ class DecryptTest < Minitest::Test
     @decrypt = Decrypt.new
   end
 
-  
+  def test_it_can_split
+    assert_equal [",", "a", "s", "h"], @decrypt.split_decryption_string
+  end
+
+  def test_it_can_assign_index_value
+    assert_equal [28,0,18,7], @decrypt.assign_index_value
+  end
 
 end
