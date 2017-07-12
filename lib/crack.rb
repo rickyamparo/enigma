a44d764434c8f1ddd6332dd9ac946511bd564a46
sidewinder2020 <sdwinder1@gmail.com>
2017-07-12 15:16:50 -0600

    Filled in more pseudo/NOT final code for crack class

--- lib/crack.rb
+++ lib/crack.rb
@@ -15,46 +15,104 @@ attr_accessor :decrypt, :key, :offsets
     @map_values_array = []
   end
 
+  def create_array
+    @map_values_array = "abcdefghijklmnopqrstuvwxyz .,".split(//)
+  end
+
   #find length of coded message
-  def 
+  def find_length
+    @output_message.length
+  end
 
   #assign ABCD, BCDA, CDAB, DABC according to % 4
+  def assign_pattern
+    if @output_message.length % 4 == 0
+      pattern ABCD
+    elsif @output_message.length % 4 == 1
+      pattern BCDA
+    elsif @output_message.length % 4 == 2
+      pattern CDAB
+    elsif @output_message.length % 4 == 3
+      patter CABC
+    end
+  end
 
-  #turn nd.. into index
+  #turn nd.. into index (PUT MAP VALUES INTO CRACK!!)
+  def end_index
+    local_array = "nd..".split(//)
+    self.create_array
+    local_array.map do |letter|
+      @map_values_array.index(letter)
+    end
+  end
 
-  #turn last 4 of message into index
+  #turn last 4 of message into index(CHANGE SAVED VARIABLE)
+    def message_4_index
+      local_array = @output_message.split(//)
+      self.create_array
+      local_array[-4..-1].map do |letter|
+        @map_values_array.index(letter)
+      end
+    end
 
   #find difference between the 2 (that's the rotation + offset)
+    def nd_index_minus_4_index
+    end
 
   #turn message into index
+    def message_to_index
+      local_array = @output_message.split(//)
+      self.create_array
+      local_array[-4..-1].map do |letter|
+        @map_values_array.index(letter)
+      end
+    end
 
   #use modulo 4 to apply ABCD pattern (like mapvaluesarray) to message
+    def apply_ABCD_patterns
+    end
 
   #according to ABCD index, apply difference to index
+    def apply_difference_to_index
+      if a_pattern
+      elsif b_pattern
+      elsif c_pattern
+      elsif d_pattern
+
+    end
 
   #convert index to map_values_array ltrs (ORIGINAL MESSAGE!!)
+    def convert_message_to_number
+      local_array = apply_difference_to_index
+      local_array.map do |number|
+        number = @map_values_array[number]
+      end
+    end
 
-  #using date, determin offset value
+  #using date, determine offset value
+    def determine_offset_value
+      use offset class to determine offset value
+    end
 
   #use difference between offset and rotation, subtract offset
+    def find_rotation
+      nd_index_minus_4_index - determine_offset_value = rotation
+    end
 
   #mess with modulo's
+    def
+    end
 
-  #reverse engineer key (ORIGINAL KEY!!)
+    def find_key
+      a =  b =  c =  d =
+      this_array=[a,b,c,d]
+      key_array = []
+      this_array.separate(separates every number)
+      this_array.find_all.evens do |number|
+        key_array << number
+      end
+      key_array << this_array.last_4
+      key_array.join
+    end
 
 end
-
-# def crack
-#   while @output_message[-7..-1] != "..end.."
-#     decrypt.create_array
-#     decrypt.split_decryption_string
-#     decrypt.assign_index_value
-#     decrypt.decide_offset_method(index)
-#     decrypt.decide_rotation_method(index)
-#     decrypt.add_rotate_to_offset(index)
-#     decrypt.subtract_rotate_and_offsets
-#     decrypt.subtract_29
-#     decrypt.create_encryption
-#     decrypt.create_encryption_string
-#   end
-#   #range of keys to try [00000..99999]
